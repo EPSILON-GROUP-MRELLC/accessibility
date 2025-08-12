fd3c8ed0c6090cca4d8ee7a987d214f3e8242fbf
Joseph Malone <admin@malonereserveequity.com>
2025-08-12 16:57:04 -0700

    added js file

---
+++ fetch-layout.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://maloneuniversity.org/partials/navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        });
+
+    fetch("https://maloneuniversity.org/partials/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+});
