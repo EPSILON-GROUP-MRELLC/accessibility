a7e7ac2cff06217113e2b6b73dfb2ffe3364db6a
Joseph Malone <admin@malonereserveequity.com>
2025-08-12 17:49:35 -0700

    fixed asynchonous loading of DOM

--- fetch-layout.js
+++ fetch-layout.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://maloneuniversity.org/partials/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar").innerHTML = data;
-        });
-
-    fetch("https://maloneuniversity.org/partials/footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer").innerHTML = data;
-        });
+    Promise.all([
+        fetch("https://maloneuniversity.org/partials/navbar.html")
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("navbar").innerHTML = data;
+            }),
+        fetch("https://maloneuniversity.org/partials/footer.html")
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("footer").innerHTML = data;
+            })
+    ]).then(() => {
+        // Load main.js after navbar & footer are inserted
+        const script = document.createElement("script");
+        script.src = "https://maloneuniversity.org/data/main.js";
+        document.body.appendChild(script);
+    });
 });
-
-
-
